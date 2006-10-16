7328f0ab3294f5e6546109a7aff256b7d2a898c4
adam <adam@4ad37cd2-5fec-0310-835f-b3785c72a374>
2006-10-16 22:35:08 +0000

    Adding basic pod to Catalyst::Action::REST Adding initial Makefile.PL

--- lib/Catalyst/Action/REST.pm
+++ lib/Catalyst/Action/REST.pm
@@ -12,7 +12,50 @@ use warnings;
 
 use base 'Catalyst::Action';
 use Class::Inspector;
+use 5.8.1;
 
+my 
+$VERSION = '0.1';
+
+=head1 NAME
+
+Catalyst::Action::REST - Automated REST Method Dispatching
+
+=head1 SYNOPSIS
+
+    sub foo :Local :ActionClass('REST') {}
+
+    sub foo_GET { 
+      ... do something for GET requests ...
+    }
+
+    sub foo_PUT { 
+      ... do somethign for PUT requests ...
+    }
+
+=head1 DESCRIPTION
+
+This Action handles doing automatic method dispatching for REST requests.  It
+takes a normal Catalyst action, and changes the dispatch to append an
+underscore and method name. 
+
+For example, in the synopsis above, calling GET on "/foo" would result in
+the foo_GET method being dispatched.
+
+If a method is requested that is not implemented, this action will 
+return a status 405 (Method Not Found).  It will populate the "Allow" header 
+with the list of implemented request methods.
+
+=head1 METHODS
+
+=over 4
+
+=item dispatch
+
+This method overrides the default dispatch mechanism to the re-dispatching
+mechanism described above.
+
+=cut
 sub dispatch {
     my ( $self, $c ) = @_;
 
@@ -48,3 +91,22 @@ sub _return_405 {
 }
 
 1;
+
+=back
+
+=head1 SEE ALSO
+
+You likely want to look at L<Catalyst::Controller::REST>, which implements
+a sensible set of defaults for a controller doing REST.
+
+L<Catalyst::Action::Serialize>, L<Catalyst::Action::Deserialize>
+
+=head1 AUTHOR
+
+Adam Jacob <adam@stalecoffee.org>, with lots of help from mst and jrockway
+
+=head1 LICENSE
+
+You may distribute this code under the same terms as Perl itself.
+
+=cut
