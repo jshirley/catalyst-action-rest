5511d1ff8e88a0c28611b903a15f513e74dab6d0
adam <adam@4ad37cd2-5fec-0310-835f-b3785c72a374>
2006-10-17 01:03:18 +0000

    Added status_created helper method

--- lib/Catalyst/Controller/REST.pm
+++ lib/Catalyst/Controller/REST.pm
@@ -3,6 +3,7 @@ package Catalyst::Controller::REST;
 use strict;
 use warnings;
 use base 'Catalyst::Controller';
+use Params::Validate qw(:all);
 
 __PACKAGE__->mk_accessors(qw(serialize));
 
@@ -17,10 +18,30 @@ __PACKAGE__->config(
     }
 );
 
-sub begin : ActionClass('Deserialize') {
-}
+sub begin : ActionClass('Deserialize') {}
 
-sub end : ActionClass('Serialize') {
+sub end : ActionClass('Serialize') { }
+
+sub status_created {
+    my $self = shift;
+    my $c = shift;
+    my %p = validate(@_,
+        {
+            location => { type => SCALAR | OBJECT },
+            entity => { optional => 1 }, 
+        },
+    );
+
+    my $location;
+    if (ref($p{'location'})) {
+        $location = $p{'location'}->as_string;
+    }
+    $c->response->status(201);
+    $c->response->header('Location' => $location);
+    if (exists($p{'entity'})) {
+        $c->stash->{$self->config->{'serialize'}->{'stash_key'}} = $p{'entity'};
+    }
+    return 1;
 }
 
 1;
